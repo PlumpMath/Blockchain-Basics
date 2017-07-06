5867436b9bd7547c0bda66850520b029628a4264
Pradeep Loganathan <pradeep@pradeeploganathan.com>
2017-07-06 13:03:58 +0530

    Folder changes

---
+++ Hashing/User Salting & Hashing/UserManagerConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UserManager;
+
+namespace UserManagerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string NameToValidate, PasswordToValidate;
+            UserManager.UserManager UsrMgr = new UserManager.UserManager();
+            Console.WriteLine("Registering new user ... ");
+            User usr = UsrMgr.RegisterNewUser(args[0], args[1]);
+            Console.WriteLine("New User {0} registered with salt of {1} and salted password of {2} .", usr.Name, usr.Salt, usr.HashedPassword);
+            Console.WriteLine("                         ------------Validating user now-----------");
+            Console.WriteLine("Enter UserName");
+            NameToValidate = Console.ReadLine();
+            Console.WriteLine("Enter Password");
+            PasswordToValidate = Console.ReadLine();
+            if (UsrMgr.ValidatePassword(NameToValidate, PasswordToValidate))
+                Console.WriteLine("Valid user -- Logging in");
+            else
+                Console.WriteLine("Invalid User");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
